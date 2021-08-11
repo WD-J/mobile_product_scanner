9bf88bb9ed0caa2ccdc5cc5b44a05bd27e500b72
William Davies <william@withutraining.com>
2021-08-11 12:43:48 +0200

    created individual store widget

---
+++ lib/ui/widgets/store_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_product_scanner/data/stores_data.dart';
+
+class StoreWidget extends StatelessWidget {
+  final Store store;
+  const StoreWidget({required this.store});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 50.0, vertical: 15.0),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: SizedBox(
+                width: 72.0,
+                height: 72.0,
+                child: Image.network(store.logoUrl),
+              ),
+            ),
+            SizedBox(
+              height: 15.0,
+            ),
+            Text(
+              store.title,
+              style: TextStyle(
+                color: Colors.black87,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              store.address + ', ' + store.city,
+              style: TextStyle(
+                color: Colors.black54,
+                fontSize: 14.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
