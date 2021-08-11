ae08e5e9f67989286329e89c298ec2ebccc2d45b
William Davies <william@withutraining.com>
2021-08-11 19:08:39 +0200

    refactored "stores" to "store"

--- lib/logic/StoreProvider.dart
+++ lib/logic/StoreProvider.dart
@@ -2,9 +2,9 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:mobile_product_scanner/data/stores_data.dart';
+import 'package:mobile_product_scanner/data/store_data.dart';
 
-class StoresProvider extends ChangeNotifier {
+class StoreProvider extends ChangeNotifier {
   List<Store> stores = [];
 
   Future<List<Store>> getStores() async {
@@ -15,9 +15,9 @@ class StoresProvider extends ChangeNotifier {
     if (response.statusCode == 200) {
       return List<Store>.from(json
           .decode(response.body)['data']
-          .map((stores) => Store.fromJson(stores)));
+          .map((store) => Store.fromJson(store)));
     } else {
-      throw Exception('Failed to load stores');
+      throw Exception('Failed to load store');
     }
   }
 
